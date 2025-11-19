680f8cf6e0fc24ae55d3a9b02f80ebcc8ab6fbef
PeterB <peterjbishop.denver@gmail.com>
2025-11-18 23:55:01 -0700

    ready for createListing

---
+++ example/lib/documents/listing/components/createListing.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateListing extends StatefulWidget {
+  const CreateListing({super.key});
+
+  @override
+  State<CreateListing> createState() => _CreateListingState();
+}
+
+class _CreateListingState extends State<CreateListing> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
